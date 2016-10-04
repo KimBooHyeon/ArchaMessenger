b586a69339ad18b1ec329e9071d2af746ef95b85
KimBooHyeon <kbh9654@gmail.com>
2016-10-04 23:29:49 +0900

    first commit

---
+++ app/src/main/java/com/archa/archamessenger/MainActivity.java
@@ -0,0 +1,79 @@
+package com.archa.archamessenger;
+
+import android.content.res.Configuration;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity implements  View.OnClickListener{
+
+
+    private DrawerLayout dlDrawer;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private LinearLayout layout_navigation;
+
+    private Button btn_menu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        layout_navigation = (LinearLayout)findViewById(R.id.layout_navigation);
+        dlDrawer = (DrawerLayout)findViewById(R.id.drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, dlDrawer, R.string.open_drawer, R.string.close_drawer){
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu();
+            }
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                invalidateOptionsMenu();
+            }
+        };
+        dlDrawer.setDrawerListener(actionBarDrawerToggle);
+
+        init();
+    }
+
+    public void init(){
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        btn_menu = (Button)findViewById(R.id.btn_menu);
+        btn_menu.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_menu:
+                dlDrawer.openDrawer(layout_navigation);
+                break;
+        }
+    }
+
+    protected void onPostCreate(Bundle savedInstanceState){
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+    }
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+}
